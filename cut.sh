7d796e6805e69a81520e0928e77b0bf6948fd3d6
Alex C. Souza <alexdcesouza@gmail.com>
2020-05-21 19:27:04 -0300

    Criação de um comando único

--- cut.sh
+++ cut.sh
@@ -5,6 +5,14 @@ INPUT_TIME_LENGHT=$2
 INPUT_FILE=$3
 #OUTPUT_FILE=$4
 SCALE=400
+TEMP_DIR = "./tmp"
 
-rm -rf frames/ && mkdir frames/ && ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i $INPUT_FILE -vf "scale=$SCALE:-1:flags=lanczos,palettegen" -y palette.png && ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i $INPUT_FILE -i palette.png -lavfi "scale=$SCALE:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y frames/temp%d.png
+rm -rf "$TEMP_DIR" 
+
+mktemp "$TEMP_DIR"
+
+ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i $INPUT_FILE -vf "scale=$SCALE:-1:flags=lanczos,palettegen" -y "$TEMP_DIR/palette.png" 
+
+ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i $INPUT_FILE -i "$TEMP_DIR/palette.png" -lavfi "scale=$SCALE:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y "$TEMP_DIR/temp%d.png"
+
 #&&rm -f palette.png

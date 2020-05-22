279cc6c7c1c813d1b0f17a28e4808b48ed5e3971
Alex C. Souza <alexdcesouza@gmail.com>
2020-05-22 08:44:12 -0300

    Melhorias

--- v2g.sh
+++ v2g.sh
@@ -5,22 +5,22 @@ INPUT_TIME_LENGHT=$2
 INPUT_FILE=$3
 OUTPUT_FILE=$4
 
-SCALE=350
+SCALE=640
 TEMP_DIR="./tmp"
 START_NUMBER=1
-CROP_BOTTOM=40
+CROP_BOTTOM=0
 
-rm -rf $TEMP_DIR || "Não foi possível remover o diretório $TEMPDIR" 
+rm -rf $TEMP_DIR || "Não foi possível remover o diretório $TEMPDIR" && "Pasta $TEMP_DIR removida."
 
-mkdir $TEMP_DIR || "Não foi possível criar o diretório $TEMP_DIR" 
+mkdir $TEMP_DIR || "Não foi possível criar o diretório $TEMP_DIR" && "Pasta $TEMP_DIR recriada."
 
-ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i $INPUT_FILE -vf "scale=$SCALE:-1:flags=lanczos,palettegen" -y "$TEMP_DIR/palette.png" 
+ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i "$INPUT_FILE" -filter_complex "crop=in_w:in_h-$CROP_BOTTOM,scale=$SCALE:-1:flags=lanczos,palettegen" -y "$TEMP_DIR/palette.png" 
 
-ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i $INPUT_FILE -i "$TEMP_DIR/palette.png" -lavfi "scale=$SCALE:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y "$TEMP_DIR/frame-%d.png.png"
+#ffmpeg -start_number $START_NUMBER -i $TEMP_DIR/frame-%d.png -filter_complex "crop=in_w:in_h-$CROP_BOTTOM,scale=$SCALE:-1:flags=lanczos,palettegen" -y $TEMP_DIR/palette.png
 
-ffmpeg -start_number $START_NUMBER -i "$TEMP_DIR/frame-%d.png.png" -filter_complex "crop=in_w:in_h-$CROP_BOTTOM,scale=$SCALE:-1:flags=lanczos,palettegen" -y "$TEMP_DIR/palette.png" 
+ffmpeg -ss $INPUT_START_TIME -t $INPUT_TIME_LENGHT -i "$INPUT_FILE" -i "$TEMP_DIR/palette.png" -lavfi "scale=$SCALE:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y $TEMP_DIR/frame-%d.png
 
-ffmpeg -start_number $START_NUMBER -i "$TEMP_DIR/frame-%d.png.png" -i "$TEMP_DIR/palette.png" -filter_complex "[0]crop=in_w:in_h-$CROP_BOTTOM,scale=$SCALE:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y "$OUTPUT_FILE" 
+ffmpeg -start_number $START_NUMBER -i $TEMP_DIR/frame-%d.png -i $TEMP_DIR/palette.png -filter_complex "[0]crop=in_w:in_h-$CROP_BOTTOM,scale=$SCALE:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y "$OUTPUT_FILE" 
 
 #rm -f "$TEMP_DIR/palette.png" 
 
